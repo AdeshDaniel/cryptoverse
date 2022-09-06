3ccc6dd84b2ce2a021869f0bcb0c7babcaa82a7b
AdeshDaniel <stockhutfxcompany@gmail.com>
2022-09-06 06:06:57 +0100

    created the layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Switch, Route, Link, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Navbar,
+  Exchanges,
+  News,
+  CryptoDetails,
+  Cryptocurrencies,
+  HomePage,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse App</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 };
